2d780419ca7dfa00445753152f2295911c19c591
Rostmaster <rotmaster007@gmail.com>
2023-10-28 13:17:39 +0300

    Password validation done

--- Lesson 3 [25-10-23]/Homework/script.js
+++ Lesson 3 [25-10-23]/Homework/script.js
@@ -1,67 +1,112 @@
-// function handle_form_submission()
-// {
-//   return matchPassword();
-// }
+window.onload = function () {
 
+    document.getElementById("submit").disabled = true;
 
-// function matchPassword() {  
-//     var password = document.getElementById("i_pass").value;  
-//     var passwordConfirmation = document.getElementById("i_pass_conf").value;  
-//     console.log(password!=""& password == passwordConfirmation);
-//     return password!=""& password == passwordConfirmation;
-//   }  
+    let passwordMessage = document.getElementById("password-message");
+    let passwordInput = document.getElementById("i_pass");
+    let nameInput = document.getElementById("i_name");
+    let aboutInput = document.getElementById("t_about");
+    let passwordMessageItems = document.getElementsByClassName("password-message-item");
+    let passwordIsCorrect = [false, false, false, false, false, false]
 
-var passwordInput = document.getElementById("i_pass"); 
-var passwordMessageItems = document.getElementsByClassName("password-message-item"); 
-var passwordMessage = document.getElementById("password-message"); 
+//? Placeholders logic
+    passwordInput.addEventListener("focus", onPassFocusIn, true);
+    passwordInput.addEventListener("blur", onPassFocusOut, true);
 
+    function onPassFocusIn() {
+        passwordInput.placeholder = 'Enter yor password';
+        passwordMessage.classList.add("visible");
+        passwordMessage.classList.remove("hide");
+    }
 
-passwordInput.onfocus = function () { 
-	passwordMessage.style.display = "block"; 
-} 
+    function onPassFocusOut() {
+        passwordInput.placeholder = '';
+        passwordMessage.classList.remove("visible");
+        passwordMessage.classList.add("hide");
+    }
 
-// After clicking outside of password input hide the message 
-passwordInput.onblur = function () { 
-	passwordMessage.style.display = "none"; 
-} 
+    nameInput.addEventListener("focus", onNameFocusIn, true);
+    nameInput.addEventListener("blur", onNameFocusOut, true);
 
-passwordInput.onkeyup = function () { 
-	// checking uppercase letters 
-	let uppercaseRegex = /[A-Z]/g; 
-	if (passwordInput.value.match(uppercaseRegex)) { 
-		passwordMessageItems[1].classList.remove("invalid"); 
-		passwordMessageItems[1].classList.add("valid"); 
-	} else { 
-		passwordMessageItems[1].classList.remove("valid"); 
-		passwordMessageItems[1].classList.add("invalid"); 
-	} 
+    function onNameFocusIn() {
+        nameInput.placeholder = 'Enter yor name';
+    }
+    function onNameFocusOut() {
+        nameInput.placeholder = '';
+    }
 
-	// checking lowercase letters 
-	let lowercaseRegex = /[a-z]/g; 
-	if (passwordInput.value.match(lowercaseRegex)) { 
-		passwordMessageItems[0].classList.remove("invalid"); 
-		passwordMessageItems[0].classList.add("valid"); 
-	} else { 
-		passwordMessageItems[0].classList.remove("valid"); 
-		passwordMessageItems[0].classList.add("invalid"); 
-	} 
+    aboutInput.addEventListener("focus", onAboutFocusIn, true);
+    aboutInput.addEventListener("blur", onAboutFocusOut, true);
 
-	// checking the number 
-	let numbersRegex = /[0-9]/g; 
-	if (passwordInput.value.match(numbersRegex)) { 
-		passwordMessageItems[2].classList.remove("invalid"); 
-		passwordMessageItems[2].classList.add("valid"); 
-	} else { 
-		passwordMessageItems[2].classList.remove("valid"); 
-		passwordMessageItems[2].classList.add("invalid"); 
-	} 
+    function onAboutFocusIn() {
+        aboutInput.placeholder = 'Tell us about yourself';
+    }
 
-	// Checking length of the password 
-	if (passwordInput.value.length >= 8) { 
-		passwordMessageItems[3].classList.remove("invalid"); 
-		passwordMessageItems[3].classList.add("valid"); 
-	} else { 
-		passwordMessageItems[3].classList.remove("valid"); 
-		passwordMessageItems[3].classList.add("invalid"); 
-	} 
-}
+    function onAboutFocusOut() {
+        aboutInput.placeholder = '';
+    }
+
+//? Inputs validation logic
+
+    passwordInput.onkeyup = function () {
+
+    //? Password validation logic
+        // checking uppercase letters 
+        let uppercaseRegex = /[A-Z]/g;
+        if (passwordInput.value.match(uppercaseRegex)) {
+            passwordMessageItems[1].classList.remove("invalid");
+            passwordMessageItems[1].classList.add("valid");
+            passwordIsCorrect[1] = true;
+        } else {
+            passwordMessageItems[1].classList.remove("valid");
+            passwordMessageItems[1].classList.add("invalid");
+            passwordIsCorrect[1] = false;
+        }
+        // 	// checking lowercase letters 
+        let lowercaseRegex = /[a-z]/g;
+        if (passwordInput.value.match(lowercaseRegex)) {
+            passwordMessageItems[0].classList.remove("invalid");
+            passwordMessageItems[0].classList.add("valid");
+            passwordIsCorrect[0] = true;
+        } else {
+            passwordMessageItems[0].classList.remove("valid");
+            passwordMessageItems[0].classList.add("invalid");
+            passwordIsCorrect[0] = false;
+        }
+
+        // checking the number 
+        let numbersRegex = /[0-9]/g;
+        if (passwordInput.value.match(numbersRegex)) {
+            passwordMessageItems[2].classList.remove("invalid");
+            passwordMessageItems[2].classList.add("valid");
+            passwordIsCorrect[2] = true;
+        } else {
+            passwordMessageItems[2].classList.remove("valid");
+            passwordMessageItems[2].classList.add("invalid");
+            passwordIsCorrect[2] = false;
+        }
+
+        // Checking length of the password 
+        if (passwordInput.value.length >= 8) {
+            passwordMessageItems[3].classList.remove("invalid");
+            passwordMessageItems[3].classList.add("valid");
+            passwordIsCorrect[3] = true;
+        } else {
+            passwordMessageItems[3].classList.remove("valid");
+            passwordMessageItems[3].classList.add("invalid");
+            passwordIsCorrect[3] = false;
+        }
+    //? Input validation logic
+    passwordIsCorrect[4] = document.getElementById("i_name").checkValidity();
+    passwordIsCorrect[5] = document.getElementById("i_pass").checkValidity();
+
+    //? Submit button enable logic
+        if (passwordIsCorrect.includes(false)) {
+            document.getElementById("submit").disabled = true;
+        } else {
+            document.getElementById("submit").disabled = false;
+        }
+        console.log(passwordIsCorrect);
+    }
+
+};
